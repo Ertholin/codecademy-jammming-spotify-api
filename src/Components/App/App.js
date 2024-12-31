864b1ef227dfe74ecfe6ab5e3d31733bad3a5dcb
Ertholin <ertholinulysse@gmail.com>
2024-12-30 23:05:03 -0500

    no-feat: Implement Adding Songs To a Custom Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,29 +4,58 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 function App() {
   const [searchResults, setSearchResults] = useState([
+    {
+      name: "Example Track Name 1",
+      artist: "Example Track Artist 1",
+      album: "Example Track Album 1",
+      id: 1,
+    },
+    {
+      name: "Example Track Name 2",
+      artist: "Example Track Artist 2",
+      album: "Example Track Album 2",
+      id: 2,
+    },
+    {
+      name: "Example Track Name 3",
+      artist: "Example Track Artist 3",
+      album: "Example Track Album 3",
+      id: 3,
+    },
   ]);
 
   const [playlistName, setPlaylistName] = useState("Example Playlist Name");
-  const [playlistTracks, setPlaylistTrack] = useState([
+  const [playlistTracks, setPlaylistTracks] = useState([
     {
       name: "Example Playlist Name 1",
       artist: "Example Playlist Artist 1",
       album: "Example Playlist Album 1",
-      id: 1,
+      id: 4,
     },
     {
       name: "Example Playlist Name 2",
       artist: "Example Playlist Artist 2",
       album: "Example Playlist Album 2",
-      id: 2,
+      id: 5,
     },
     {
       name: "Example Playlist Name 3",
       artist: "Example Playlist Artist 3",
       album: "Example Playlist Album 3",
-      id: 3,
+      id: 6,
     },
   ]);
+
+  function addTrack(track) {
+    const existingTrack = playlistTracks.find((t) => t.id === track.id);
+    const newTrack = playlistTracks.concat(track);
+    if (existingTrack) {
+      console.log("Track already exists: " + existingTrack.name);
+    } else {
+      setPlaylistTracks(newTrack);
+    }
+  }
+
   return (
     <div>
       <h1>
@@ -37,7 +66,7 @@ function App() {
 
         <div className={styles['App-playlist']}>
           {/* <!-- Add a SearchResults component --> */}
-          <SearchResults userSearchResults={searchResults} />
+          <SearchResults userSearchResults={searchResults} onAdd={addTrack}/>
 
           {/* <!-- Add a Playlist component --> */}
           <Playlist playlistName={playlistName} playlistTracks={playlistTracks} />
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,21 +1,32 @@
 import React from "react";
 import styles from "./Track.module.css";
 
-function Track (props) {
+function Track(props) {
   function renderAction() {
-    return <button className={styles['Track-action']}>{props.isRemoval ? "-" : "+"}</button>;
+    if (props.isRemoval) {
+      return <button className={styles['Track-action']} onClick={passTrack}>
+        +
+      </button>;
+    } else {
+      return <button className={styles['Track-action']}>-</button>;
+    }
   }
-    return (
-      <div className={styles.Track}>
-        <div className={styles['Track-information']}>
-          {/* <h3><!-- track name will go here --></h3> */}
-          <h3>{props.track.name}</h3>
-          {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
-          <p>{props.track.artist} | {props.track.album}</p>
-        </div>
-        {/* <button class="Track-action"><!-- + or - will go here --></button> */}
+
+  function passTrack() {
+    props.onAdd(props.track);
+  }
+  return (
+    <div className={styles.Track}>
+      <div className={styles['Track-information']}>
+        {/* <h3><!-- track name will go here --></h3> */}
+        <h3>{props.track.name}</h3>
+        {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+        <p>{props.track.artist} | {props.track.album}</p>
       </div>
-    );
+      {/* <button class="Track-action"><!-- + or - will go here --></button> */}
+      {renderAction()}
+    </div>
+  );
 }
 
 export default Track;
